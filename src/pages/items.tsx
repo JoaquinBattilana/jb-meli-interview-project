615bd4da14833b175bfd57dc1482d390456ae569
JoaquinBattilana <battilanajn@gmail.com>
2020-02-27 05:17:23 -0300

    created basic view for product and productlist

---
+++ src/pages/items.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ProductsListView() {
+  return <h1>Esta es la vista de los items!</h1>;
+}
+
+export default ProductsListView;
---
+++ src/pages/items/[id].tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ProductView() {
+  return <h1>Esta es la vista del producto en particular!</h1>;
+}
+
+export default ProductView;
