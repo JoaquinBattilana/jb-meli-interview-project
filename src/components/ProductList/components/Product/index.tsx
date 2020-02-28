851e957eb26a070f9376b9c05a0fdbba3b17229e
JoaquinBattilana <battilanajn@gmail.com>
2020-02-28 07:42:12 -0300

    created basic currency layout

--- src/components/ProductList/components/Product/index.tsx
+++ src/components/ProductList/components/Product/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import styles from './styles.module.scss';
 
-function ProductList() {
-  return (<h1>Hola!</h1>);
+interface PropTypes {
+  id: number,
+  title: string,
+  price: {
+    currency: string,
+    amount: number,
+    decimals: number
+  },
+  decimals: number,
+  condition: string,
+  free_shipping: boolean,
+  picture: string
+}
+
+function ProductList({
+  id,
+  title,
+  price,
+  condition,
+  free_shipping,
+  picture
+} : PropTypes) {
+  return (
+    <div className={styles['product-container']}>
+      <img src={picture} />
+      <h2>{title}</h2>
+    </div>
+  );
 }
 
 export default ProductList;
