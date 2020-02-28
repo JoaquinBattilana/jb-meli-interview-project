397e5aac4770c2cacf39ff2e55f2f5b388f8dcf5
JoaquinBattilana <battilanajn@gmail.com>
2020-02-28 04:41:53 -0300

    created actions for getting items with async dispatchs

--- src/redux/items/actions.js
+++ src/redux/items/actions.js
@@ -1,12 +1,24 @@
+import itemsService from '~services/itemsService';
+
 export const actions = {
-  GET_ITEMS: '@@ITEMS/GET_ITEMS'
+  GET_ITEMS: '@@ITEMS/GET_ITEMS',
+  GET_ITEMS_SUCESS: '@@ITEMS/GET_ITEMS_SUCESS',
+  GET_ITEMS_FAIL: '@@ITEMS/GET_ITEMS_FAIL'
 };
 
 const actionsCreators = {
-  getItems: query => ({
-    type: actions.GET_ITEMS,
-    payload: query
-  })
+  getItems: query => async dispatch => {
+    dispatch({ type: actions.LOGIN });
+    const response = await itemsService.getItems(query);
+    if (response.ok) {
+      dispatch({
+        type: actions.LOGIN_SUCESS,
+        payload: response.data
+      });
+    } else {
+      dispatch({ type: actions.LOGIN_FAIL });
+    }
+  }
 };
 
 export default actionsCreators;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,17 @@
-import { createStore, combineReducers, compose } from 'redux';
+import {
+  createStore,
+  combineReducers,
+  compose,
+  applyMiddleware
+} from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
   form: formReducer
 });
 
+const enhancers = [applyMiddleware(thunk)];
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-const makeStore = initialState => createStore(rootReducer, initialState, composeEnhancers());
+const makeStore = initialState => createStore(rootReducer, initialState, composeEnhancers(...enhancers));
 export default makeStore;
