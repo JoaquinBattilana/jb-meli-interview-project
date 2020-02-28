135c76fc186b2e04ab7d4c67037db872b54fe16a
JoaquinBattilana <battilanajn@gmail.com>
2020-02-28 00:03:52 -0300

    created types for forms in an appart file

--- src/components/Navbar/components/SearchForm/index.tsx
+++ src/components/Navbar/components/SearchForm/index.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Field, reduxForm, InjectedFormProps } from 'redux-form';
+import { SearchFormType } from '~types/formTypes';
 import styles from './styles.module.scss';
-
-interface SearchForm {
-  searchField: string
-}
 
 interface PropTypes {
   className: string;
 }
 
-function SearchForm({ handleSubmit, className } : PropTypes & InjectedFormProps<SearchForm>) {
+function SearchForm({ handleSubmit, className } : PropTypes & InjectedFormProps<SearchFormType>) {
   return (
     <form onSubmit={handleSubmit} className={`${className} ${styles.form}`}>
       <button type="submit" className={styles['submit-button']}>
